43a8f2161ce9c1766f047b716ecc57b7ced27c8b
rager520 <1254078534@qq.com>
2018-07-25 23:23:09 +0800

    补行为模式

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,6 +1,8 @@
-﻿using DesignPattern.Creational;
+﻿using DesignPattern.Behavioral;
+using DesignPattern.Creational;
 using DesignPattern.Structural;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -35,7 +37,30 @@ namespace DesignPattern
             //RunFlyWeight();
             //RunFlyWeightRealWorld();
             //RunProxy();
-            RunProxyRealWorld();
+            //RunProxyRealWorld();
+
+            //RunChainofResp();
+            //RunChainofRespRealWorld();
+            //RunCommand();
+            //RunCommandRealWorld();
+            //RunInterpreter();
+            //RunInterpreterRealWorld();
+            //RunIterator();
+            //RunIteratorRealWorld();
+            //RunMediator();
+            //RunMediatorRealWorld();
+            //RunMemento();
+            //RunMementoRealWorld();
+            //RunObserver();
+            //RunObserverRealWorld();
+            //RunState();
+            //RunStateRealWorld();
+            //RunStrategy();
+            //RunStrategyRealWorld();
+            //RunTemplateMethod();
+            //RunTemplateMethodRealWorld();
+            //RunVisitor();
+            RunVisitorRealWorld();
 
             // Wait for user input
             Console.ReadKey();
@@ -214,91 +239,363 @@ namespace DesignPattern
 
         static void RunChainofResp()
         {
+            // 职责链 更在于链式结构  类与类之间存在依赖关系
+            Handler h1 = new ConcreteHandler1();
+            Handler h2 = new ConcreteHandler2();
+            Handler h3 = new ConcreteHandler3();
+            h1.SetSuccessor(h2);
+            h2.SetSuccessor(h3);
 
+            // Generate and process request
+            int[] requests = { 2, 5, 14, 22, 18, 3, 27, 20 };
+
+            foreach (int request in requests)
+            {
+                h1.HandleRequest(request);
+            }
         }
         static void RunChainofRespRealWorld()
         {
+            // Setup Chain of Responsibility
+            Approver larry = new ChainofRespDirector();
+            Approver sam = new VicePresident();
+            Approver tammy = new President();
+
+            larry.SetSuccessor(sam);
+            sam.SetSuccessor(tammy);
+
+            // Generate and process purchase requests
+            Purchase p = new Purchase(2034, 350.00, "Assets");
+            larry.ProcessRequest(p);
+
+            p = new Purchase(2035, 32590.10, "Project X");
+            larry.ProcessRequest(p);
+
+            p = new Purchase(2036, 122100.00, "Project Y");
+            larry.ProcessRequest(p);
 
         }
         static void RunCommand()
         {
+            // 发起者 命令 接收者 三者脱离
+            // Create receiver, command, and invoker
+            Receiver receiver = new Receiver();
+            SimpleCommand command = new ConcreteCommand(receiver);
+            Invoker invoker = new Invoker();
 
+            // Set and execute command
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
         }
         static void RunCommandRealWorld()
         {
+            // Create user and let her compute
+            User user = new User();
 
+            // User presses calculator buttons
+            user.Compute('+', 100);
+            user.Compute('-', 50);
+            user.Compute('*', 10);
+            user.Compute('/', 2);
+
+            // Undo 4 commands
+            user.Undo(4);
+
+            // Redo 3 commands
+            user.Redo(3);
         }
         static void RunInterpreter()
         {
+            // 解释器在于有个全局类
+            InterpreterContext context = new InterpreterContext();
 
+            // Usually a tree 
+            ArrayList list = new ArrayList();
+
+            // Populate 'abstract syntax tree' 
+            list.Add(new TerminalExpression());
+            list.Add(new NonterminalExpression());
+            list.Add(new TerminalExpression());
+            list.Add(new TerminalExpression());
+
+            // Interpret
+            foreach (AbstractExpression exp in list)
+            {
+                exp.Interpret(context);
+            }
         }
         static void RunInterpreterRealWorld()
         {
+            string roman = "MCMXXVIII";
+            Context context = new Context(roman);
 
+            // Build the 'parse tree'
+            List<Expression> tree = new List<Expression>();
+            tree.Add(new ThousandExpression());
+            tree.Add(new HundredExpression());
+            tree.Add(new TenExpression());
+            tree.Add(new OneExpression());
+
+            // Interpret
+            foreach (Expression exp in tree)
+            {
+                exp.Interpret(context);
+            }
+
+            Console.WriteLine("{0} = {1}",
+              roman, context.Output);
         }
         static void RunIterator()
         {
+            // 对象的一个集合
+            ConcreteAggregate a = new ConcreteAggregate();
+            a[0] = "Item A";
+            a[1] = "Item B";
+            a[2] = "Item C";
+            a[3] = "Item D";
 
+            // Create Iterator and provide aggregate
+            SimpleIterator i = a.CreateIterator();
+
+            Console.WriteLine("Iterating over collection:");
+
+            object item = i.First();
+            while (item != null)
+            {
+                Console.WriteLine(item);
+                item = i.Next();
+            }
         }
         static void RunIteratorRealWorld()
         {
+            // Build a collection
+            Collection collection = new Collection();
+            collection[0] = new Item("Item 0");
+            collection[1] = new Item("Item 1");
+            collection[2] = new Item("Item 2");
+            collection[3] = new Item("Item 3");
+            collection[4] = new Item("Item 4");
+            collection[5] = new Item("Item 5");
+            collection[6] = new Item("Item 6");
+            collection[7] = new Item("Item 7");
+            collection[8] = new Item("Item 8");
 
+            // Create iterator
+            Iterator iterator = collection.CreateIterator();
+
+            // Skip every other item
+            iterator.Step = 2;
+
+            Console.WriteLine("Iterating over collection:");
+
+            for (Item item = iterator.First();
+                !iterator.IsDone; item = iterator.Next())
+            {
+                Console.WriteLine(item.Name);
+            }
         }
         static void RunMediator()
         {
+            // 中介含有所有具体的操作类 每个类含有一个中介
+            ConcreteMediator m = new ConcreteMediator();
 
+            ConcreteColleague1 c1 = new ConcreteColleague1(m);
+            ConcreteColleague2 c2 = new ConcreteColleague2(m);
+
+            m.Colleague1 = c1;
+            m.Colleague2 = c2;
+
+            c1.Send("How are you?");
+            c2.Send("Fine, thanks");
         }
         static void RunMediatorRealWorld()
         {
+            Chatroom chatroom = new Chatroom();
 
+            // Create participants and register them
+            Participant George = new Beatle("George");
+            Participant Paul = new Beatle("Paul");
+            Participant Ringo = new Beatle("Ringo");
+            Participant John = new Beatle("John");
+            Participant Yoko = new NonBeatle("Yoko");
+
+            chatroom.Register(George);
+            chatroom.Register(Paul);
+            chatroom.Register(Ringo);
+            chatroom.Register(John);
+            chatroom.Register(Yoko);
+
+            // Chatting participants
+            Yoko.Send("John", "Hi John!");
+            Paul.Send("Ringo", "All you need is love");
+            Ringo.Send("George", "My sweet Lord");
+            Paul.Send("John", "Can't buy me love");
+            John.Send("Yoko", "My sweet love");
         }
         static void RunMemento()
         {
+            // 可以理解用一个类来储存另一个类状态
+            Originator o = new Originator();
+            o.State = "On";
 
+            // Store internal state
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+
+            // Continue changing originator
+            o.State = "Off";
+
+            // Restore saved state
+            o.SetMemento(c.Memento);
         }
         static void RunMementoRealWorld()
         {
+            SalesProspect s = new SalesProspect();
+            s.Name = "Noel van Halen";
+            s.Phone = "(412) 256-0990";
+            s.Budget = 25000.0;
 
+            // Store internal state
+            ProspectMemory m = new ProspectMemory();
+            m.Memento = s.SaveMemento();
+
+            // Continue changing originator
+            s.Name = "Leo Welch";
+            s.Phone = "(310) 209-7111";
+            s.Budget = 1000000.0;
+
+            // Restore saved state
+            s.RestoreMemento(m.Memento);
         }
         static void RunObserver()
         {
+            // 一对多 一个变化 通知多个
+            // Configure Observer pattern
+            ConcreteSubject s = new ConcreteSubject();
 
+            s.Attach(new ConcreteObserver(s, "X"));
+            s.Attach(new ConcreteObserver(s, "Y"));
+            s.Attach(new ConcreteObserver(s, "Z"));
+
+            // Change subject and notify observers
+            s.SubjectState = "ABC";
+            s.Notify();
         }
         static void RunObserverRealWorld()
         {
+            IBM ibm = new IBM("IBM", 120.00);
+            ibm.Attach(new Investor("Sorros"));
+            ibm.Attach(new Investor("Berkshire"));
 
+            // Fluctuating prices will notify investors
+            ibm.Price = 120.10;
+            ibm.Price = 121.00;
+            ibm.Price = 120.50;
+            ibm.Price = 120.75;
         }
         static void RunState()
         {
+            // 有个全局环境类 用以检测状态变化
+            // Setup context in a state
+            StateContext c = new StateContext(new ConcreteStateA());
 
+            // Issue requests, which toggles state
+            c.Request();
+            c.Request();
+            c.Request();
+            c.Request();
         }
         static void RunStateRealWorld()
         {
+            // Open a new account
+            Account account = new Account("Jim Johnson");
 
+            // Apply financial transactions
+            account.Deposit(500.0);
+            account.Deposit(300.0);
+            account.Deposit(550.0);
+            account.PayInterest();
+            account.Withdraw(2000.00);
+            account.Withdraw(1100.00);
         }
         static void RunStrategy()
         {
+            // 一个全局类
+            StrategyContext context;
 
+            // Three contexts following different strategies
+            context = new StrategyContext(new ConcreteStrategyA());
+            context.ContextInterface();
+
+            context = new StrategyContext(new ConcreteStrategyB());
+            context.ContextInterface();
+
+            context = new StrategyContext(new ConcreteStrategyC());
+            context.ContextInterface();
         }
         static void RunStrategyRealWorld()
         {
+            // Two contexts following different strategies
+            StragegySortedList studentRecords = new StragegySortedList();
 
+            studentRecords.Add("Samual");
+            studentRecords.Add("Jimmy");
+            studentRecords.Add("Sandra");
+            studentRecords.Add("Vivek");
+            studentRecords.Add("Anna");
+
+            studentRecords.SetSortStrategy(new QuickSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStrategy(new ShellSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStrategy(new MergeSort());
+            studentRecords.Sort();
         }
         static void RunTemplateMethod()
         {
+            AbstractClass aA = new ConcreteClassA();
+            aA.TemplateMethod();
 
+            AbstractClass aB = new ConcreteClassB();
+            aB.TemplateMethod();
         }
         static void RunTemplateMethodRealWorld()
         {
+            DataAccessObject daoCategories = new Categories();
+            daoCategories.Run();
 
+            DataAccessObject daoProducts = new Products();
+            daoProducts.Run();
         }
         static void RunVisitor()
         {
+            // 多对多的关系
+            // Setup structure
+            ObjectStructure o = new ObjectStructure();
+            o.Attach(new ConcreteElementA());
+            o.Attach(new ConcreteElementB());
 
+            // Create visitor objects
+            ConcreteVisitor1 v1 = new ConcreteVisitor1();
+            ConcreteVisitor2 v2 = new ConcreteVisitor2();
+
+            // Structure accepting visitors
+            o.Accept(v1);
+            o.Accept(v2);
         }
         static void RunVisitorRealWorld()
         {
+            // Setup employee collection
+            Employees e = new Employees();
+            e.Attach(new Clerk());
+            e.Attach(new VisitorDirector());
+            e.Attach(new VisitorPresident());
 
+            // Employees are 'visited'
+            e.Accept(new IncomeVisitor());
+            e.Accept(new VacationVisitor());
         }
 
         #endregion
